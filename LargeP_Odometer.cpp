c213be98247bd834e88981eb4ef4d24937c3c44a
Andrew Shallue <ashallue@iwu.edu>
2022-07-13 00:19:02 -0500

    I think I'm done with LargeP_Odometer.  Lots of debugging.  I've tested with a relatively small B, would be nice to create a sort function so I can test it on larger B

--- LargeP_Odometer.cpp
+++ LargeP_Odometer.cpp
@@ -6,9 +6,9 @@ using namespace std;
 
 #include "LargeP_Odometer.h"
 
-// For now, this constructor is focused on Carmichaels with 3 prime factors
+// Sets up all the variables.
 LargeP_Odometer::LargeP_Odometer(){
-  // let's focus on 3 prime factors
+  // let's start with 3 prime factors
   curr_d = 3;
   P_len = 1;
 
@@ -28,7 +28,22 @@ LargeP_Odometer::LargeP_Odometer(){
   }
   factor_sieve(nums, prime_B);
   primes = primes_fromfs(nums, prime_B);
-  
+ 
+  // Calculate max_d.  
+  bigint prod = 1;
+  max_d = 1;  //start at p = 3;
+  while(prod < B && max_d < prime_B){
+    prod = prod * primes.at(max_d);
+    max_d++;
+  }
+  // subtract one to get it back less than B
+  max_d--;
+
+  // allocate memory for arrays
+  uppers = new long[max_d];
+  lowers = new long[max_d];
+  indices = new long[max_d];
+ 
   //For the bounds, intialize first to all 0's
   for(long i = 0; i < max_d; ++i){
     uppers[i] = 0;
@@ -47,6 +62,22 @@ LargeP_Odometer::LargeP_Odometer(){
   // except that first index is the smallest prime greater than X
   indices[0] = find_index_lower(X);
   P_curr = primes[ indices[0] ];
+
+  // but there might not be such primes, in which case we move on to length-2 pre-products
+  if(indices[0] == 0) next_nextd();
+}
+
+LargeP_Odometer::~LargeP_Odometer(){
+  delete[] uppers;
+  delete[] lowers;
+  delete[] indices;
+}
+
+// Construct odometer with given curr_d.  I only intend to use this as a helper, so primes not filled in.
+LargeP_Odometer::LargeP_Odometer(long d){
+  // set d and P_len
+  curr_d = d;
+  P_len = curr_d - 2;
 }
 
 // helper function.  Given lower bound, find index of the smallest prime larger than the bound
@@ -106,6 +137,61 @@ long LargeP_Odometer::find_index_lower(long bound){
   return curr_index;
 }
 
+// helper function.  Assuming we have finished pre-products for Carmichael numbers with current length,
+// reset for the next length.  This involves updated variables and changing the indices array
+void LargeP_Odometer::next_nextd(){
+  // update curr_d and P_len
+  curr_d++;   P_len++;
+
+  // set primes up to P_len - 1 to be minimum possible, P_len prime is minimum given lower bound
+  // Also calculate product of the pre-product up to that point, and update the lower bounds
+  bigint prod = 1;
+  for(long i = 0; i < P_len - 1; ++i){
+    indices[i] = i+1;
+    prod = prod * primes[ indices[i] ];
+    lowers[i] =   primes[ indices[i] ];
+  }
+  // The final lower bound is either X / prod, or q_{P_len-2}, whichever is bigger
+  long last_lower = X / prod ; 
+  if(last_lower < primes[ indices[ P_len - 2] ]){
+    indices[P_len - 1] = indices[P_len - 2] + 1;
+  }else{
+    long last_index = find_index_lower(last_lower);
+    indices[P_len - 1] = last_index;
+  }
+
+  /* At this point there are two failure modes I can think of.
+   * (1) prod * last_prime might be smaller than X, not >=.  This I can check and fix.
+   * (2) indices[P_len - 1] = 0, meaning there was no prime in the set large enough.
+   */
+  // With the planned for bounds, I don't think (2) will happen.  Check just in case
+  if(indices[P_len - 1] == 0){
+    cout << "Problem in next_nextd(), no prime large enough to fill out pre-product\n";
+  }else{
+    // fix for (1): add 1 to index until pre-product is large enough
+    while(primes[ indices[P_len - 1] ] * prod < X){
+      indices[P_len - 1] = indices[P_len - 1] + 1;
+    }
+    // update last lower bound
+    lowers[P_len - 1] = primes[ indices[P_len - 1] ];
+  } 
+  prod = 1; 
+  long bound;
+  // update the upper bounds.  (B / prod(i))^{1 / (d - i)}
+  for(long i = 0; i < P_len; ++i){
+    bound = ceil(pow( B/prod, 1.0 / (curr_d - i) ));
+    uppers[i] = bound;
+    prod = prod * primes[ indices[i] ]; 
+  }
+  // update the current pre-product
+  P_curr = 1;
+  for(long i = 0; i < P_len; ++i){
+    P_curr = P_curr * primes[ indices[i] ];
+  }
+
+  return;
+}
+
 // retrieve current pre-product.  A simple getter function
 bigint LargeP_Odometer::get_P(){
   return P_curr;
@@ -113,7 +199,192 @@ bigint LargeP_Odometer::get_P(){
     
 // Rotate the indices.  Starts with furthest right index, depending on P_len
 void LargeP_Odometer::next(){
+  // index used to find the correct index to change.  Start at the furthest right
+  long curr_index = P_len - 1;
+  // next prime after the one at the current index
+  long next_prime;
+  // boolean value to identify when an index is maxed out
+  bool max = false;
+  // variable for calculating pre-products
+  bigint prod;
+  long bound;
+
+  // work backwards from the last index.  Check if that index is maxed out already.
+  // Reasons it could be maxed out:  (1) next prime is above upper bound, (2) curr index at end of array
+  // I don't think curr index will be zero.  My plan is to check for that later in this function
+  if(indices[curr_index] == (primes.size() - 1)){
+    max = true;
+  }else{
+    // now I know it is safe to grab the next prime
+    next_prime = primes[ indices[curr_index] + 1 ];
+    if(next_prime >= uppers[curr_index]) max = true;
+  }
+  // while maxed, subtract one from curr_index and check again
+  while(max){
+    curr_index--;
+
+    // check if curr_index is now -1.  If so, we have finished this this curr_d, move on to next one
+    if(curr_index == -1){
+      max = false;
+    
+    // otherwise proceed with the same logic as before
+    }else if(indices[curr_index] == (primes.size() - 1)){
+      max = true;
+    }else{
+      next_prime = primes[ indices[curr_index] + 1 ];
+      if(next_prime >= uppers[curr_index]){
+        max = true;
+      }else{
+        max = false;
+      }
+    }
+  } // end while
+  // at this point I have found the correct index to increment, or all indices maxed out
+  // in the latter case, call next_nextd() to move on to Carmichaels with more prime factors
+  if(curr_index == -1){
+    next_nextd();
+  }else{
+    indices[curr_index]++;
+
+    // if curr_index is P_len - 1, all we need to do at this point is update P_curr
+    if(curr_index == P_len - 1){
+      P_curr = P_curr / primes[ indices[curr_index] - 1 ];
+      P_curr = P_curr * primes[ indices[curr_index]     ];
+    }else{
+
+      // calculate pre-product up to curr_index
+      prod = 1; 
+      for(long i = 0; i < curr_index + 1; ++i){
+        prod = prod * primes[ indices[i] ];
+      }
+      // we now need to move forward and update lower and upper bounds 
+      for(long i = curr_index + 1; i < P_len - 1; ++i){
+        bound = ceil(pow( B/prod, 1.0 / (curr_d - i) ));
+        uppers[i] = bound;
+        lowers[i] = primes[ indices[i] + 1 ];
+
+        // update the index: one more than the previous position
+        indices[i] = indices[i-1] + 1; 
+        prod = prod * primes[ indices[i] ];
+
+      } // end for
+      // update bounds on the final prime
+      bound = ceil(pow( B/prod, 1.0 / 3 ));
+      uppers[P_len - 1] = bound;
+   
+      // final lower bound is either X/prod or q_{P_len - 2}, whichever is bigger
+      bound = X / prod;
+      if(bound < primes[ indices[ P_len - 2 ] ]){
+        indices[P_len - 1] = indices[P_len - 2] + 1;
+      }else{
+        long last_index = find_index_lower(bound);
+        indices[P_len - 1] = last_index;
+      }     
+      lowers[P_len - 1] = primes[ indices[P_len - 1] ];
+      
+      // update pre-product
+      P_curr = prod * primes[ indices[P_len - 1] ];
+
+    } // end else  
+  } // end else
   return;
 } 
 
+// print the entries of the indices array, the uppers array, and the lowers array
+void LargeP_Odometer::print_info(){
+  cout << "indices: ";
+  for(long i = 0; i < max_d; ++i){
+    cout << indices[i] << " ";
+  }
+  cout << "\n";
+  cout << "upper bounds: ";
+  for(long i = 0; i < max_d; ++i){
+    cout << uppers[i] << " ";
+  }
+  cout << "\n";
+  cout << "lower bounds: ";
+  for(long i = 0; i < max_d; ++i){
+    cout << lowers[i] << " ";
+  }
+  cout << "\n";
+  return;
+}
+
+// repeatedly call next() to compute all pre-products and write them to a file.  
+// Stops when the product of the first d primes is greater than B
+void LargeP_Odometer::large_products(string filename){
+  // set up the file
+  ofstream output;
+  output.open(filename);
+
+  bigint preprod;
+  // continue until current d value is greater than the max allowed
+  while(curr_d < max_d){
+    preprod = get_P();
+    
+    // write the preproduct along with its factors
+    output << preprod << ": ";
+    for(long i = 0; i < P_len; ++i){
+      output << primes[ indices[i] ] << " ";
+    }     
+    output << "\n";
+     
+    // next pre-product
+    next(); 
+ 
+  } // end while
+  output.close();
+  return;
+}
+
+// A trivial algorithm for tabulating large pre-products.  Outputs P to a file given by filename
+void LargeP_Odometer::trivial_large_products(string filename){
+  // set up the flie
+  ofstream output;
+  output.open(filename);
+
+  // set up the factor sieve
+  long nums[B];
+  for(long i = 0; i < B; i++){
+    nums[i] = i;
+  }
+  factor_sieve(nums, B);
+
+  vector<long> prime_factors;
+  bool include_it;
+  bigint prod;
+  long biggest_prime;
+
+  // n is a pre-product
+  for(bigint n = 3; n < B; ++n){
+    // factor n
+    prime_factors = unique_prime_divs(n, nums, B);
+
+    // check if square_free
+    prod = 1;
+    for(long i = 0; i < prime_factors.size(); ++i){
+      prod *= prime_factors.at(i);
+    }
+    // keep it if it is squarefree, if it has at most max_d-2, factors is odd
+    if(prod == n && prime_factors.size() <= max_d - 2 && n % 2 == 1){
+      
+      // find the biggest prime 
+      biggest_prime = prime_factors.at(0);
+      for(long i = 1; i < prime_factors.size(); ++i){
+        if(prime_factors.at(i) > biggest_prime) biggest_prime = prime_factors.at(i);
+      }
+
+      // if the remaining pre-product is between X and P p_{d-2}^2 < B
+      if(prod >= X && prod * biggest_prime * biggest_prime < B){
+        output << prod << ": ";
+        for(long i = 0; i < prime_factors.size(); ++i){
+          output << prime_factors.at(i) << " ";
+        }
+        output << "\n";
+      }
+    }
+  } 
+
+  return;
+}
 
--- LargeP_Odometer.h
+++ LargeP_Odometer.h
@@ -1,6 +1,7 @@
 /* Andrew Shallue, Tabulating Carmichaels project. 2022
  * The goal of this class is to construct all pre-products greater than a bound.
  * Helper class for the case of tabulating Carmichaels with large pre-product.
+ * Important!  We assume the lower bound is at most 64 bits, but note pre-products can be greater than that..
  *
  * This class works under the assumption that the bound on the Carmichaels constructed is at most 128 bits
  */
@@ -14,17 +15,22 @@ using namespace std;
 #include "functions.h"
 #include <vector>
 #include <math.h>
+#include <fstream>
+#include <string>
 
 class LargeP_Odometer{
   public:
-    static const long max_d = 25;   // At most 25 distinct primes can be multiplied and stay within 128 bits
+    // max_d is the max num of distinct primes that can be multiplied up to B
+    // Note that I will make room for d primes in the pre-product, though we only need d-2
+    // Note that at most 25 distinct primes can be multiplied and fit in 128 bits
+    long max_d;
     long curr_d;  // count of prime factors of n.  count for P is d-2
     long P_len;     // count of prime factors of P.  We have P_len = curr_d - 2
     vector<long> primes; // vector of primes of size X = B^{1/3}   
-    long uppers[max_d];  // upper bounds on prime factors
-    long lowers[max_d];  // lower bounds on prime factors
+    long* uppers;  // upper bounds on prime factors
+    long* lowers;  // lower bounds on prime factors
 
-    long indices[max_d]; // stores the indices of the primes dividing current pre-product
+    long* indices; // stores the indices of the primes dividing current pre-product
     bigint P_curr;       // current pre-product that corresponds to the index array
   
   public:
@@ -33,20 +39,40 @@ class LargeP_Odometer{
     // minimum bound that works is 3, otherwise will go into an infinite loop since no prime < 2
     long find_index_lower(long bound);
 
+    // helper function.  Assuming we have finished pre-products for Carmichael numbers with current length,
+    // reset for the next length.  This involves updated variables and changing the indices array
+    void next_nextd();
+
   public:
     bigint B;     // bound on Carmichaels constructed
-    bigint X;     // lower bound on pre-products P such that n = Pqr
+    long X;       // lower bound on pre-products P such that n = Pqr
     long prime_B; // bound on the prime factors of P
 
     // For now, this constructor orients around Carmichaels with three prime factors
     LargeP_Odometer();
-    
+  
+    // destructor releases memory for the arrays;
+    ~LargeP_Odometer();
+ 
+    // Construct odometer with given curr_d.  I only intend to use this as a helper, so primes not filled in.
+    // not sure I need it.  Unfinished for now.
+    LargeP_Odometer(long d);
+ 
     // Rotate the indices.  Starts with furthest right index, depending on P_len
     void next(); 
 
     // retrieve current pre-product.  A simple getter function
     bigint get_P();
-  
+
+    // print the entries of the indices array, the uppers array, and the lowers array
+    void print_info();
+
+    // repeatedly call next() to compute all pre-products and write them to a file.  
+    // Stops when the product of the first d primes is greater than B
+    void large_products(string filename);
+ 
+    // A trivial algorithm for tabulating large pre-products.  Outputs P to a file given by filename
+    void trivial_large_products(string filename);
 };
 
 #endif
