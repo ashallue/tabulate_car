be5572ae0cd7d43e92584775358262a9e3739faf
Andrew Shallue <ashallue@iwu.edu>
2023-12-15 09:58:04 -0600

    created a cars8_threaded function.  Haven't tested it yet.  Still want to do a cars9_threaded, then let recursive take care of the rest of the cases

--- LP_extra.cpp
+++ LP_extra.cpp
@@ -575,3 +575,479 @@ void LargePreproduct::cars7_threaded(string cars_file, long thread, long num_thr
 
   output.close();
 }
+
+
+// threaded version of cars8
+void LargePreproduct::cars8_threaded(string cars_file, long thread, long num_threads){
+  //setup file
+  ofstream output;
+  output.open(cars_file);
+
+  // primes out of the primes index, their indices
+  long p1, p2, p3, p4, p5, p6, q;
+  long i1, i2, i3, i4, i5, i6, i7;
+  // lower bounds are given in terms of index, uppers in terms of values
+  long lower_index;
+  long upper1, upper2, upper3, upper4, upper5, upper6, upper7;
+
+  // keep running computation of P and lcm_p|P p-1
+  bigint P1, P2, P3, P4, P5, P6, P7;
+  bigint L1, L2, L3, L4, L5, L6, L7;
+  long g;
+
+  vector<long> rs;
+
+  // nested for loops
+  // compute first upper bound as B^{1/8}
+  upper1 = find_upper(B, 1, 8);
+  //cout << "upper1 = " << upper1 << "\n";
+
+  // start p1 at the prime corresponding to thread number
+  // Update: new threading.  All threads consider same primes, but only enter inner loop
+  // if num_admissable is in a certain class
+  long num_admissable = 0;  
+
+  i1 = 0;
+  p1 = primes[i1];
+  P1 = p1;
+  do{
+
+    // compute L1
+    L1 = p1 - 1;
+
+    // take p2 to be next prime after p1
+    i2 = i1 + 1;
+
+    // also need to compute the corresponding upper bound: (B/p1)^{1/7}
+    upper2 = find_upper(B, p1, 7);
+    //cout << "then lower_index = " << lower_index << " and upper2 = " << upper2 << "\n";
+
+    // finding the start index for p2
+    p2 = primes[i2];
+    // check admissability, bump ahead until found
+    while( gcd( p2 - 1, P1) != 1){
+      i2++;
+      p2 = primes[i2];
+    }
+    P2 = P1 * p2;
+
+    do{
+
+      //update L2
+      L2 = L1 * (p2 - 1);
+      g = gcd(L1, p2 - 1);
+      L2 = L2 / g;
+
+      // take p3 to be next prime after p2 
+      i3 = i2 + 1;
+
+      // upper bound is (B/p1p2)^{1/6}
+      upper3 = find_upper(B, P2, 6);
+      
+      // find start index for p3
+      p3 = primes[i3];
+      // check admissability
+      while( gcd(p3 - 1, P2) != 1){
+        i3++;
+        p3 = primes[i3];
+      }
+      P3 = P2 * p3;
+
+      do{
+
+        // update L3
+        L3 = L2 * (p3 - 1);
+        g = gcd(L2, p3 - 1);
+        L3 = L3 / g;
+
+        // take p4 to be next prime after p3
+        i4 = i3 + 1;
+
+        // upper bound is (B/p1p2p3)^{1/5}
+        upper4 = find_upper(B, P3, 5);
+
+        // find start index for p4
+        p4 = primes[i4];
+        // check admissability
+        while( gcd(p4 - 1, P3) != 1){
+          i4++;
+          p4 = primes[i4];
+        }
+        P4 = P3 * p4;
+
+        do{
+           
+          // update L4
+          L4 = L3 * (p4 - 1);
+          g = gcd(L3, p4 - 1);
+          L4 = L4 / g;
+
+          // take p5 to be next prime, compute upper bound as (B/p1p2p3p4)^{1/4}
+          i5 = i4 + 1;
+          upper5 = find_upper(B, P4, 4);
+
+          // check admissability to find start index for p5
+          p5 = primes[i5];
+          while( gcd(p5 - 1, P4) != 1){
+            i5++;
+            p5 = primes[i5];
+          }
+          P5 = P4 * p5; 
+
+          do{
+
+            // check threading
+            // only do the p6 work if correct thread
+            if(num_admissable % num_threads == thread){
+
+            // update L5
+            L5 = L4 * (p5 - 1);
+            g = gcd(L4, p5 - 1);
+            L5 = L5 / g;
+
+            // if p1 * p2 * p3 * p4 * p5^2 > X take i6 = i5 + 1.  Otherwise X / p1p2p3p4p5
+            if(P5 * p5 > X){
+              lower_index = i5 + 1;
+            }else{
+              lower_index = find_index_lower( X / P5 );
+            }
+            i6 = lower_index;
+
+            // upper bound is (B/p1p2p3p4p5)^{1/3}
+            upper6 = find_upper(B, P5, 3);
+
+            // find start index for p6, discarding choices not admissable
+            p6 = primes[i6];
+            while( gcd(p6 - 1, P5) != 1 ){
+              i6++;
+              p6 = primes[i6];
+            }
+            P6 = P5 * p6;
+
+            do{
+              // update L6
+              L6 = L5 * (p6 - 1);
+              g = gcd(L5, p6 - 1);
+              L6 = L6 / g;
+
+              // lower bound for q is just the previous prime, upper is (B/p1p2p3p4p5p6)^{1/2}
+              upper7 = find_upper(B, P6, 2);
+        
+              // finding the start index and prime for q
+              i7 = i6 + 1;
+              q = primes[i7];
+              // check admissability, bump ahead until found
+              while( gcd( q - 1, P6 ) != 1 ){
+                i7++;
+                q = primes[i7];
+              }
+              P7 = P6 * q;
+
+              do{
+                // update L7
+                L7 = L6 * (q - 1);
+                g = gcd(L6, q - 1);
+                L7 = L7 / g;
+
+                //cout << "Inner loop with p1 = " << p1 << " p2 = " << p2 << " p3 = " << p3 << " p4 = " << p4 << " p5 = " << p5 << " q = " << q << "\n";
+
+                // complicated inner loop work that finds r's that make carmichaels
+                // clears rs vector and refills it
+                inner_loop_work(P7, q, L7, rs);
+
+                // write to file
+                for(long i = 0; i < rs.size(); i++){
+                  // note this next line might attempt to print a bigint and faile
+                  output << P7 * rs[i] << " ";
+                  output << p1 << " " << p2 << " " << p3 << " " << p4 << " " << p5 << " " << p6 << " " << q << " " << rs[i] << "\n";
+                }
+
+                // find next q that makes preproduct admissable
+                do{
+                  i7++;
+                  q = primes[i7];
+                }while( gcd( q - 1, P6 ) != 1 );
+                P7 = P6 * q;
+
+              } while(q < upper7); // end of do q
+
+              // find next p6
+              do{
+                i6++;
+                p6 = primes[i6];
+              }while( gcd( p6 - 1, P5 ) != 1 );
+              P6 = P5 * p6;
+
+            }while(p6 < upper6); // end of do p6
+            } // end if admissalbe in a certain thread
+ 
+            // find next p5
+            do{
+              i5++;
+              p5 = primes[i5];
+            }while( gcd( p5 - 1, P4 ) != 1);
+            P5 = P4 * p5;
+
+            num_admissable++;
+
+          }while(p5 < upper5);  // end of do p5
+
+          // find next p4
+          do{
+            i4++;
+            p4 = primes[i4];
+          }while( gcd( p4 - 1, P3 ) != 1);
+          P4 = P3 * p4;
+        
+        }while(p4 < upper4); // end of do p4
+
+        // find next p3 that makes p1*p2*p3 admissable
+        do{
+          i3++;
+          p3 = primes[i3];
+        }while( gcd( p3 - 1, P2 ) != 1 );
+        P3 = P2 * p3;
+
+      }while(p3 < upper3);  // end of do p3
+
+      // find next p2 that makes p1*p2 admissable
+      do{
+        i2++;
+        p2 = primes[i2];
+      }while( gcd( p2 - 1, P1 ) != 1 );
+      P2 = P1 * p2;
+
+    }while(p2 < upper2);  // end of do p2
+
+    // next prime p1
+    i1 += 1;
+    p1 = primes[i1];
+    P1 = p1;
+  }while(p1 < upper1);  // end of do p1
+
+  output.close();
+}
+
+
+// threaded version of cars9
+void LargePreproduct::cars9_threaded(string cars_file, long thread, long num_threads){
+  //setup file
+  ofstream output;
+  output.open(cars_file);
+
+  // primes out of the primes index, their indices
+  long p1, p2, p3, p4, p5, q;
+  long i1, i2, i3, i4, i5, i6;
+  // lower bounds are given in terms of index, uppers in terms of values
+  long lower_index;
+  long upper1, upper2, upper3, upper4, upper5, upper6;
+
+  // keep running computation of P and lcm_p|P p-1
+  bigint P1, P2, P3, P4, P5, P6;
+  bigint L1, L2, L3, L4, L5, L6;
+  long g;
+
+  vector<long> rs;
+
+  // nested for loops
+  // compute first upper bound as B^{1/7}
+  upper1 = find_upper(B, 1, 7);
+  //cout << "upper1 = " << upper1 << "\n";
+
+  // start p1 at the prime corresponding to thread number
+  // Update: new threading.  All threads consider same primes, but only enter inner loop
+  // if num_admissable is in a certain class
+  long num_admissable = 0;  
+
+  i1 = 0;
+  p1 = primes[i1];
+  P1 = p1;
+  do{
+
+    // compute L1
+    L1 = p1 - 1;
+
+    // take p2 to be next prime after p1
+    i2 = i1 + 1;
+
+    // also need to compute the corresponding upper bound: (B/p1)^{1/6}
+    upper2 = find_upper(B, p1, 6);
+    //cout << "then lower_index = " << lower_index << " and upper2 = " << upper2 << "\n";
+
+    // finding the start index for p2
+    p2 = primes[i2];
+    // check admissability, bump ahead until found
+    while( gcd( p2 - 1, P1) != 1){
+      i2++;
+      p2 = primes[i2];
+    }
+    P2 = P1 * p2;
+
+    do{
+
+      //update L2
+      L2 = L1 * (p2 - 1);
+      g = gcd(L1, p2 - 1);
+      L2 = L2 / g;
+
+      // take p3 to be next prime after p2 
+      i3 = i2 + 1;
+
+      // upper bound is (B/p1p2)^{1/5}
+      upper3 = find_upper(B, P2, 5);
+      
+      // find start index for p3
+      p3 = primes[i3];
+      // check admissability
+      while( gcd(p3 - 1, P2) != 1){
+        i3++;
+        p3 = primes[i3];
+      }
+      P3 = P2 * p3;
+
+      do{
+
+        // update L3
+        L3 = L2 * (p3 - 1);
+        g = gcd(L2, p3 - 1);
+        L3 = L3 / g;
+
+        // take p4 to be next prime after p3
+        i4 = i3 + 1;
+
+        // upper bound is (B/p1p2p3)^{1/4}
+        upper4 = find_upper(B, P3, 4);
+
+        // find start index for p4
+        p4 = primes[i4];
+        // check admissability
+        while( gcd(p4 - 1, P3) != 1){
+          i4++;
+          p4 = primes[i4];
+        }
+        P4 = P3 * p4;
+
+        do{
+
+          // check threading
+          // only do the p5 work if correct thread
+          if(num_admissable % num_threads == thread){
+
+          // update L4
+          L4 = L3 * (p4 - 1);
+          g = gcd(L3, p4 - 1);
+          L4 = L4 / g;
+
+          // if p1 * p2 * p3 * p4^2 > X take i5 = i4 + 1.  Otherwise X / p1p2p3p4
+          if(P4 * p4 > X){
+            lower_index = i4 + 1;
+          }else{
+            lower_index = find_index_lower( X / P4 );
+          }
+          i5 = lower_index;
+
+          // upper bound is (B/p1p2p3p4)^{1/3}
+          upper5 = find_upper(B, P4, 3);
+
+          // find start index for p5, discarding choices not admissable
+          p5 = primes[i5];
+          while( gcd(p5 - 1, P4) != 1 ){
+            i5++;
+            p5 = primes[i5];
+          }
+          P5 = P4 * p5;
+
+          do{
+            // update L5
+            L5 = L4 * (p5 - 1);
+            g = gcd(L4, p5 - 1);
+            L5 = L5 / g;
+
+            // lower bound for q is just the previous prime, upper is (B/p1p2p3p4p5)^{1/2}
+            upper6 = find_upper(B, P5, 2);
+        
+            // finding the start index and prime for q
+            i6 = i5 + 1;
+            q = primes[i6];
+            // check admissability, bump ahead until found
+            while( gcd( q - 1, P5 ) != 1 ){
+              i6++;
+              q = primes[i6];
+            }
+            P6 = P5 * q;
+
+            do{
+              // update L6
+              L6 = L5 * (q - 1);
+              g = gcd(L5, q - 1);
+              L6 = L6 / g;
+
+              //cout << "Inner loop with p1 = " << p1 << " p2 = " << p2 << " p3 = " << p3 << " p4 = " << p4 << " p5 = " << p5 << " q = " << q << "\n";
+
+              // complicated inner loop work that finds r's that make carmichaels
+              // clears rs vector and refills it
+              inner_loop_work(P6, q, L6, rs);
+
+              // write to file
+              for(long i = 0; i < rs.size(); i++){
+                // note this next line might attempt to print a bigint and faile
+                output << P6 * rs[i] << " ";
+                output << p1 << " " << p2 << " " << p3 << " " << p4 << " " << p5 << " " << q << " " << rs[i] << "\n";
+              }
+
+              // find next q that makes P2 * q admissable
+              do{
+                i6++;
+                q = primes[i6];
+              }while( gcd( q - 1, P5 ) != 1 );
+              P6 = P5 * q;
+
+            } while(q < upper6); // end of do q
+
+            // find next p5
+            do{
+              i5++;
+              p5 = primes[i5];
+            }while( gcd( p5 - 1, P4 ) != 1 );
+            P5 = P4 * p5;
+
+          }while(p5 < upper5); // end of do p5
+          } // end if admissalbe in a certain thread
+ 
+          // find next p4
+          do{
+            i4++;
+            p4 = primes[i4];
+          }while( gcd( p4 - 1, P3 ) != 1);
+          P4 = P3 * p4;
+        
+          num_admissable++;
+ 
+        }while(p4 < upper4); // end of do p4
+
+        // find next p3 that makes p1*p2*p3 admissable
+        do{
+          i3++;
+          p3 = primes[i3];
+        }while( gcd( p3 - 1, P2 ) != 1 );
+        P3 = P2 * p3;
+
+      }while(p3 < upper3);  // end of do p3
+
+      // find next p2 that makes p1*p2 admissable
+      do{
+        i2++;
+        p2 = primes[i2];
+      }while( gcd( p2 - 1, P1 ) != 1 );
+      P2 = P1 * p2;
+
+    }while(p2 < upper2);  // end of do p2
+
+    // next prime p1
+    i1 += 1;
+    p1 = primes[i1];
+    P1 = p1;
+  }while(p1 < upper1);  // end of do p1
+
+  output.close();
+}
