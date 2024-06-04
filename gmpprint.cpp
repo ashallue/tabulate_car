de654e6a89716f558efaeb8d9d69e00610189cc7
Andrew Shallue <ashallue@iwu.edu>
2024-06-04 17:38:05 -0500

    took a while to write a function that parses a line of the form n factors, but finally got it.  Function is called parse_mpz and is found in gmpprint.h

--- gmpprint.cpp
+++ gmpprint.cpp
@@ -19,6 +19,49 @@ ostream & operator<<(ostream & os, const mpz_t x){
   return os;
 }
 
+// function that takes a string made up of strings of digits separated by spaces
+// I assume that the first number is an arbitrarily long n, and the rest are smaller factors
+void parse_mpz(string char_nums, mpz_t &n, vector<bigint> &factors){
+
+  // repeatedly do the following until the string is empty:
+  // find the position of the next space, grab the number as a substring, convert to c_string and then to mpz
+  int index = 0;
+  int space_pos;
+  string num_str;
+  int count = 0;
+  
+  while(index < char_nums.length()){
+    // find the space
+    space_pos = char_nums.find(' ', index);
+    cout << "in while loop, index = " << index << " and space_pos = " << space_pos << "\n";
+
+    // substring args are start position, length of substring
+    // if space_pos = -1, it means no space found, so go until the end of the string
+    if(space_pos == -1){
+      num_str = char_nums.substr(index);
+    }else{
+      num_str = char_nums.substr(index, space_pos - index);
+    }
+
+    // if count is 0, this is the first num, set n.  Otherwise add to the vector
+    if(count == 0){
+      mpz_set_str(n, num_str.c_str(), 10); 
+    }else{
+      factors.push_back(string_to_bigint(num_str));      
+    }
+    count++;
+     
+    // update index.  If space_pos is -1, we are at the end of the string
+    if(space_pos == -1){
+      index = char_nums.length();
+    }else{
+      index = space_pos + 1;
+    }
+  }
+}
+
+/* This was an idea I had for a read function, but I was getting ambiguity errors.
+// Possibly it is the correct idea, but conflicts with an existing GMP function.
 istream & operator>>(istream & is, mpz_t &x){
   // allocate memory for a c-string that can fit 1000 digits
   const int gmplen = 1002;
@@ -41,6 +84,7 @@ istream & operator>>(istream & is, mpz_t &x){
   // now set the mpz and return the stream
   mpz_set_str(x, gmpintstr, 10);
   return is;
-
+  
 }
+*/
 
--- gmpprint.h
+++ gmpprint.h
@@ -8,15 +8,17 @@
 #include <fstream>
 #include <iostream>
 #include "gmp.h"
+#include <vector>
+#include "bigint.h"
 
 using namespace std;
 
 // overload the << operator to deal with mpz_t
-
-
 ostream & operator<<(ostream & os, const mpz_t x);
 
-istream & operator>>(istream & is, mpz_t &x);
+// function that takes a string made up of strings of digits separated by spaces
+// I assume that the first number is an arbitrarily long n, and the rest are smaller factors
+void parse_mpz(string char_nums, mpz_t &n, vector<bigint> &factors);
 
 #endif
 
